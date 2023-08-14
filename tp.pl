517fa2d7a9bcc61c1cdfdca415b26346858b3e6c
Matias <matiasfernandezlucero@gmail.com>
2023-08-13 22:22:15 -0300

    Fixes y puntos faltantes

--- tp.pl
+++ tp.pl
@@ -1,8 +1,4 @@
-/*TP Logíco Grupo 3 
-  Matías Ezequiel Fernández Lucero 
-  Gustavo DG Gutiérrez */
-
-%comio(Personaje, Bicho) 
+% TP Logíco Grupo 3 | Matías Ezequiel Fernández Lucero | Gustavo DG Gutiérrez %
 
 comio(pumba, vaquitaSanAntonio(gervasia,3)). 
 comio(pumba, hormiga(federica)). 
@@ -24,7 +20,6 @@ comio(shenzi,hormiga(conCaraDeSimba)).
 
 pesoHormiga(2). 
 
-%peso(Personaje, Peso) 
 peso(pumba, 100). 
 peso(timon, 50). 
 peso(simba, 200). 
@@ -36,51 +31,106 @@ peso(banzai, 500).
 
 % Punto 1%
 % A %
-jugosita(Cucaracha) :- comio(Quien, Cucaracha),
-                findall(Cucaracha,comio(Quien, Cucaracha), Lista).
-cucaracha(Nombre,T1,P1),cucarqacha(Nombre,T2,P2), T1 = T2, P1 > P2.
-cucaracha(Nombre,T1,P1),cucarqacha(Nombre,T2,P2), T1 = T2, P1 < P2.  
+
+% Inversibilidad %
+jugosita(Cucaracha) :-
+    comio(_, cucaracha(_, Tamanio, Peso)),
+    comio(_, Cucaracha),
+    Cucaracha = cucaracha(_, Tamanio, PesoCucaracha),
+    PesoCucaracha > Peso.
+
 % B %
-hormigofilico(Quien) :- comio(Quien, hormiga(_)).
+% Inversibilidad %
+hormigofilico(Personaje) :-
+    findall(Personaje, (comio(Personaje, hormiga(A)), comio(Personaje, hormiga(B)), A \= B), Personajes),
+    member(Personaje, Personajes).
 
 % C %
-/* Aca creo el predicado personaje(Quien) como generador para paoder hacer inversible al predicado not, admitiendo consultas
-individuales como existenciales */
-cucarachofobico(Quien) :-personaje(Quien), not(comio(Quien,cucaracha(_,_,_))).
-personaje(Quien) :- comio(Quien, _).
+% Inversibilidad %
+cucarachofobico(Personaje) :-
+    findall(Personaje, (comio(Personaje, _), not(comio(Personaje, cucaracha(_, _, _)))), Personajes),
+    member(Personaje, Personajes).
+
 
 % D %
+% Inversibilidad %
+
 picaron(Quien) :- comio(Quien,vaquitaSanAntonio(remeditos,_)).
+picaron(pumba).
 picaron(Quien) :- comio(Quien, Cucaracha), jugosita(Cucaracha).
 picarones(Lista) :- findall(Quien, picaron(Quien), Lista).
 
 % Punto 2 %
+
 persigue(scar, timon). 
 persigue(scar, pumba). 
 persigue(shenzi, simba). 
 persigue(shenzi, scar). 
 persigue(banzai, timon). 
-
 persigue(scar, mufasa). 
 
-% A %
-cuantoEngorda(Personaje, Peso) :-  comio(Personaje, Bicho), cuantoPesa(Bicho, Peso).
 
-cuantoPesa(vaquitaSanAntonio(_,Peso), Peso), 
-cuantoPesa(cucaracha(_,_,Peso), Peso),
-cuantoPesa(pesoHormiga(Peso), Peso).
+% A %
+
+pesoBicho(vaquitaSanAntonio(_, Peso), Peso).
+pesoBicho(cucaracha(_, _, Peso), Peso).
+pesoBicho(hormiga(_),2).
+
+% Recursividad %
+sumarListas([], 0).
+sumarListas([X | Xs], Suma) :-
+    sumarListas(Xs, SumaRestante),
+    Suma is X + SumaRestante.
+
+% Inversibilidad %
+engordadoConBichos(Personaje,PesoFinal):-
+    peso(Personaje,_),
+    findall(Peso,(comio(Personaje,Bicho),pesoBicho(Bicho,Peso)),Pesos),
+    sumarListas(Pesos,Acumulador),
+    PesoFinal is Acumulador.
+    
+% Inversibilidad %
+engordadoConPresas(Personaje,PesoResultante):-
+    peso(Personaje,_),
+    findall(Peso+PesoPresa+PesoDepredador,(persigue(Personaje,Presa),peso(Presa,PesoPresa),engordadoConBichos(Presa,Peso),engordadoConPresas(Presa,PesoDepredador)),Pesos),
+    sumarListas(Pesos,Acumulador),
+    PesoResultante is Acumulador.
+
+% Inversibilidad %
+cuantoEngorda(Personaje,PesoFinal):-
+    peso(Personaje,_),
+    engordadoConBichos(Personaje,Peso1),
+    engordadoConPresas(Personaje,Peso2),
+    PesoFinal is Peso1+Peso2. 
 
 % Punto 3 %
-
+% Inversibilidad %
 bichosComio(Bicho, Cantidad) :- comio(Personaje, Bicho),
-                        findall(Bicho, comio(Personaje, Bicho), Bichos).
+                        findall(Bicho, comio(Personaje, Bicho), Bichos),
                         length(Bichos, Cantidad).
 
+% Inversibilidad %
 animalesPerseguidos(Animal, Cantidad) :- persigue(Personaje, Animal),
-                        findall(Animal, persigue(Personaje, Animal), Animales).
+                        findall(Animal, persigue(Personaje, Animal), Animales),
                         length(Animales, Cantidad).
 
 
+% Punto 4 %
+
+% Inversibilidad %
+cantidadDeDepredadores(Personaje,CantidadDeDepredadores):-
+    persigue(_,Personaje),
+    findall(Depredador,persigue(Depredador,Personaje),Depredadores),
+    length(Depredadores,CantidadDeDepredadores).
+
+% Inversibilidad %
+rey(Personaje):-
+    persigue(_,Personaje),
+    not(comio(Personaje,_)),
+    not(persigue(Personaje,_)),
+    cantidadDeDepredadores(Personaje,NumeroDepredadores),
+    NumeroDepredadores == 1.
+    
 
 
 
